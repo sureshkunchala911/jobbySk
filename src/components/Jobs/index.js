cf6c3a0c63fcdc591de591927f4a3c96cbfa7795
root <sureshkunchala911@gmail.com>
2022-09-08 13:27:03 +0000

    jobby

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header'
+import JobProfile from '../JobProfile'
+import './index.css'
+
+const Jobs = () => (
+  <>
+    <Header />
+    <div className="job-profile-container">
+      <JobProfile />
+    </div>
+  </>
+)
+
+export default Jobs
